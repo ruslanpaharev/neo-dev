43d4531a4c93b2f5af9213ad9570c6aaa6adf6bb
Ruslan <ruslanpaharev@gmail.com>
2019-04-30 01:07:42 +0400

    delete

--- spring/08_paging_sorting_query/src/main/java/ru/neoflex/dev/spring/paging_sorting_query/MyEntityRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.neoflex.dev.spring.paging_sorting_query;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MyEntityRepository extends CrudRepository<MyEntity, Long> {
-}
